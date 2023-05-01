7759e5bb140062c8e54957f574cd46a650a0ac89
M-Taha-Dev <hasnain221059@gmail.com>
2023-05-01 18:44:11 +0500

    task9

--- lib/Tasks/task9.dart
+++ lib/Tasks/task9.dart
@@ -1,10 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+
+
 class task9 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text('Task 9'),
+    return MaterialApp(
+      title: 'Video Player Demo',
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late VideoPlayerController _controller;
+  bool _isPlaying = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.network(
+        'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4')
+      ..initialize().then((_) {
+        setState(() {});
+      });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  void _togglePlayPause() {
+    setState(() {
+      _isPlaying = !_isPlaying;
+      if (_isPlaying) {
+        _controller.play();
+      } else {
+        _controller.pause();
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+     
+      body: Center(
+        child: _controller.value.isInitialized
+            ? AspectRatio(
+          aspectRatio: _controller.value.aspectRatio,
+          child: VideoPlayer(_controller),
+        )
+            : CircularProgressIndicator(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _togglePlayPause,
+        child: Icon(_isPlaying ? Icons.pause : Icons.play_arrow),
       ),
     );
   }
