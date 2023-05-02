44284612c531bca7c9f5226ca50acd340f3dbf12
M-Taha-Dev <hasnain221059@gmail.com>
2023-05-03 00:19:44 +0500

    task1

--- lib/Tasks/task1.dart
+++ lib/Tasks/task1.dart
@@ -1,11 +1,83 @@
 import 'package:flutter/material.dart';
+
 class task1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text('Task 1'),
+    return MaterialApp(
+      title: 'My App',
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 200,
+              color: Colors.blue,
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text('Button 1'),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text('Button 2'),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Container(
+              height: 200,
+              color: Colors.red,
+            ),
+            SizedBox(height: 20),
+            GridView.count(
+              crossAxisCount: 2,
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              children: [
+                Container(
+                  color: Colors.green,
+                  child: Center(
+                    child: Text('Grid Item 1'),
+                  ),
+                ),
+                Container(
+                  color: Colors.orange,
+                  child: Center(
+                    child: Text('Grid Item 2'),
+                  ),
+                ),
+                Container(
+                  color: Colors.purple,
+                  child: Center(
+                    child: Text('Grid Item 3'),
+                  ),
+                ),
+                Container(
+                  color: Colors.yellow,
+                  child: Center(
+                    child: Text('Grid Item 4'),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
+      drawer: Drawer(),
     );
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,6 +10,7 @@ import firebase_auth
 import firebase_core
 import flutter_local_notifications
 import flutter_secure_storage_macos
+import shared_preferences_foundation
 import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -18,5 +19,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
