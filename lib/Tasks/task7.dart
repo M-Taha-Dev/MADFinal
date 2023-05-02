3fbdeb687ebd5ce5f0cbe5d1e8241350b99f2f45
M-Taha-Dev <hasnain221059@gmail.com>
2023-05-03 01:28:33 +0500

    task7

--- lib/Tasks/task7.dart
+++ lib/Tasks/task7.dart
@@ -1,10 +1,61 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+
 class task7 extends StatelessWidget {
+  const task7({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text('Task 7'),
+    String ImageURL = '';
+    return MaterialApp(
+      title: 'Image Upload Screen',
+      home: Scaffold(
+          appBar: AppBar(),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () async {
+                    //code here
+                    ImagePicker imagepicker = ImagePicker();
+                    XFile? file = await imagepicker.pickImage(source: ImageSource.gallery);
+
+                    if (file == null)
+                    {
+                      print("In IF");
+                      return;
+                    }
+                    else{
+                      print("In else");
+                      print("File Path: ");
+                      print(file?.path.toString());
+
+
+                      Reference referenceRoot = FirebaseStorage.instance.ref();
+                      Reference referenceDIRImages = referenceRoot.child('images');
+
+                      Reference referenceImagetoUpload = referenceDIRImages.child('${file?.name}');
+
+                      try{
+                        await referenceImagetoUpload.putFile(File(file!.path));
+                        ImageURL = await referenceImagetoUpload.getDownloadURL();
+                      }catch(error)
+                      {
+                        print("FIle not Uploaded: $error");
+                      }
+                    }
+
+                  },
+                  child: Text("Upload Image"),
+
+                ),
+              ],
+            ),
+          )
+
       ),
     );
   }
