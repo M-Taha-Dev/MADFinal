45f6a1307d6aec59083809e1f1790e123b3b6f57
M-Taha-Dev <hasnain221059@gmail.com>
2023-05-03 00:55:27 +0500

    task 6

--- lib/Tasks/task6.dart
+++ lib/Tasks/task6.dart
@@ -1,11 +1,78 @@
 import 'package:flutter/material.dart';
-class task6 extends StatelessWidget {
+import 'package:shared_preferences/shared_preferences.dart';
+
+class task6 extends StatefulWidget {
+  @override
+  State<task6> createState() => _task6State();
+}
+
+class _task6State extends State<task6> {
+  var nameController = TextEditingController();
+  String? nameValue = 'No Value Saved';
+
+  @override
+  void initState() {
+    super.initState();
+    getSharedPrefs();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text('Task 6'),
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      appBar: AppBar(
+        titleSpacing: 21,
+        backgroundColor: const Color(0xFF0D47A1),
+        shadowColor: Colors.deepPurpleAccent,
+        elevation: 21,
+        title: const Text('Custom Widget'),
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: 50,),
+              Container(
+                child: Center(
+                  child: SizedBox(
+                    width: 200,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TextField(
+                          controller: nameController,
+                          decoration: InputDecoration(
+                              label: const Text('Name'),
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(21))),
+                        ),
+                        const SizedBox(
+                          height: 11,
+                        ),
+                        ElevatedButton(
+                            onPressed: () async {
+                              var prefs = await SharedPreferences.getInstance();
+                              prefs.setString(
+                                  'name', nameController.text.toString());
+                            },
+                            child: const Text('Save')),
+                        Text('Shared Preferences Value: ${nameValue}')
+                      ],
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
+  }
+
+  void getSharedPrefs() async {
+    var prefs = await SharedPreferences.getInstance();
+    nameValue = prefs.getString('name');
+    setState(() {});
   }
 }
